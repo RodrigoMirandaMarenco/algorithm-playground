88993b4c209dc854f09df94b356810112c7bd8a4
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2022-03-29 23:00:08 -0600

    ✨ Add findKthSmallestNumberEfficient method to KthUtils class

--- src/main/java/KthUtils.java
+++ src/main/java/KthUtils.java
@@ -17,4 +17,39 @@ public class KthUtils {
         result = sortedNums[k - 1];
         return result;
     }
+
+    public static int findKthSmallestNumberEfficient(int[] nums, int k) {
+        return findKthSmallestNumberRecursive(nums, k, 0, nums.length - 1);
+    }
+
+    private static int findKthSmallestNumberRecursive(int[] nums, int k, int start, int end) {
+        int p = partition(nums, start, end);
+        if (p == k - 1) {
+            return nums[p];
+        }
+        if (p > k - 1) {
+            return findKthSmallestNumberRecursive(nums, k, start, p - 1);
+        }
+        return findKthSmallestNumberRecursive(nums, k, p + 1, end);
+    }
+
+    private static int partition(int[] nums, int low, int high) {
+        if (low == high)
+            return low;
+
+        int pivot = nums[high];
+        for (int i = low; i < high; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, low++, i);
+            }
+        }
+        swap(nums, low, high);
+        return low;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
