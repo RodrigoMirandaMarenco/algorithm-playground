cfc7d2ccf459dceb04ad96ae0b4b5939c030a8ec
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2022-03-29 23:00:54 -0600

    ✅ Add kth_efficient_isCorrect test

--- src/test/java/KthUtilsTest.java
+++ src/test/java/KthUtilsTest.java
@@ -6,8 +6,13 @@ public class KthUtilsTest {
 
     @Test
     public void kth_isCorrect() {
-        // since there are two 5s in the input array, our 3rd and 4th smallest numbers should be a '5'
-        int result = KthUtils.findKthSmallestNumber(new int[]{5, 12, 11, -1, 12}, 3); // { -1, 5, 11, 12, 12 }
+        int result = KthUtils.findKthSmallestNumber(new int[]{5, 12, 11, -1, 12}, 3);
+        assertEquals(11, result);
+    }
+
+    @Test
+    public void kthEfficient_isCorrect() {
+        int result = KthUtils.findKthSmallestNumberEfficient(new int[]{5, 12, 11, -1, 12}, 3);
         assertEquals(11, result);
     }
 }
